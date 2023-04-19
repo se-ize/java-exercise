2843be495120710183eab971462ea9ebc1ef79e0
se-ize <seizenim@gmail.com>
2023-04-19 14:19:34 +0900

    Fix: add JavaInput, JavaInputTest

---
+++ src/main/java/com/example/javaproject2/JavaInputTest.java
@@ -0,0 +1,10 @@
+package com.example.javaproject2;
+
+import java.io.IOException;
+
+public class JavaInputTest {
+    public static void main(String[] args) throws IOException {
+        JavaInput javaInput = new JavaInput();
+        javaInput.readTwoChars();
+    }
+}
