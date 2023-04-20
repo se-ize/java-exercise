ff2b5b1f7e6467caaa2969293542f2ddb06d9b2a
se-ize <seizenim@gmail.com>
2023-04-20 09:30:44 +0900

    Fix: add AddNumber, AddNumberTest

---
+++ src/main/java/com/example/javaproject2/AddNumber.java
@@ -0,0 +1,10 @@
+package com.example.javaproject2;
+
+import java.util.Scanner;
+
+public class AddNumber {
+    public void plusTwoNums2() {
+        Scanner sc =new Scanner(System.in);
+        System.out.println(sc.nextInt() + sc.nextInt());
+    }
+}
---
+++ src/main/java/com/example/javaproject2/AddNumberTest.java
@@ -0,0 +1,8 @@
+package com.example.javaproject2;
+
+public class AddNumberTest {
+    public static void main(String[] args) {
+        AddNumber addNumber = new AddNumber();
+        addNumber.plusTwoNums2();
+    }
+}
