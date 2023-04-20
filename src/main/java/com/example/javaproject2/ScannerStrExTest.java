b53f00b1193854ce23b9d93f401af347200964dd
se-ize <seizenim@gmail.com>
2023-04-20 09:41:54 +0900

    Fix: add ScannerStrEx, ScannerStrExTest

---
+++ src/main/java/com/example/javaproject2/ScannerStrExTest.java
@@ -0,0 +1,8 @@
+package com.example.javaproject2;
+
+public class ScannerStrExTest {
+    public static void main(String[] args) {
+        ScannerStrEx scannerStrEx = new ScannerStrEx();
+        scannerStrEx.readStr();
+    }
+}
