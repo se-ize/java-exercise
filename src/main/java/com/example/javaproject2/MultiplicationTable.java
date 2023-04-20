e21276373d0e7725e7485e78b2ad5f09edee0377
se-ize <seizenim@gmail.com>
2023-04-20 13:13:52 +0900

    Fix: 구구단 2단

--- src/main/java/com/example/javaproject2/MultiplicationTable.java
+++ src/main/java/com/example/javaproject2/MultiplicationTable.java
@@ -1,8 +1,9 @@
 package com.example.javaproject2;
+import java.util.Scanner;
 
 public class MultiplicationTable {
     public static void main(String[] args) {
-        for (int i = 1; i < 10; i++) {
+        for (int i = 2; i < 3; i++) {
             for (int j = 1; j < 10; j++) {
                 System.out.printf("%d * %d = %d\n", i, j ,i * j);
             }
