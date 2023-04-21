b9dac599cb44755dddd262e3a159004d7dbc9340
se-ize <seizenim@gmail.com>
2023-04-21 15:29:56 +0900

    Fix: add User, UserTest

---
+++ src/main/java/com/example/javaproject2/User.java
@@ -0,0 +1,11 @@
+package com.example.javaproject2;
+
+public class User {
+    String name;
+    String phoneNumber;
+    int age;
+
+    boolean isAdult() {
+        return age >= 18;
+    } //oop적인 코딩
+}
---
+++ src/main/java/com/example/javaproject2/UserTest.java
@@ -0,0 +1,23 @@
+package com.example.javaproject2;
+
+public class UserTest {
+    public static void main(String[] args) {
+        User user1 = new User();
+        user1.name = "김서현";
+        user1.phoneNumber = "010-0000-0730";
+        user1.age = 23;
+        System.out.printf("%s님은 성인입니까? %s\n", user1.name, user1.isAdult());
+
+        User user2 = new User();
+        user2.name = "정세운";
+        user2.phoneNumber = "010-0000-0531";
+        user2.age = 25;
+        System.out.printf("%s님은 성인입니까? %s\n", user2.name, user2.isAdult());
+
+        User user3 = new User();
+        user3.name = "이혜인";
+        user3.phoneNumber = "010-0000-0000";
+        user3.age = 14;
+        System.out.printf("%s님은 성인입니까? %s\n", user3.name, user3.isAdult());
+    }
+}
