b7adb007aedbd245be9c6d5fb035b227accdec45
se-ize <seizenim@gmail.com>
2023-04-21 14:32:20 +0900

    Fix: fix SpaceInvaderMemberVariable

--- src/main/java/com/example/javaproject2/SpaceInvaderMemberVariable.java
+++ src/main/java/com/example/javaproject2/SpaceInvaderMemberVariable.java
@@ -3,13 +3,16 @@ package com.example.javaproject2;
 public class SpaceInvaderMemberVariable {
     int location; //멤버변수, 초기화 안해도 됨
     public final int MAX_LEVEL = 3;
-    private void moveLeft() {
+    public void moveLeft() {
         location = location -1;
+
+    }
+    public void moveLeftAndPrint() {
+        location = location - 1;
         System.out.printf("moveLeft: %d\n", location);
     }
-    private void moveRight() {
+    public void moveRight() {
         location = location + 1;
-        System.out.printf("moveRight: %d\n", location);
     }
 
     public static void main(String[] args) {
