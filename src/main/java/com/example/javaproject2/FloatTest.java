b2aae814ff1358c4cb9e64a7cbfc08ae01df92bc
se-ize <seizenim@gmail.com>
2023-04-24 11:40:25 +0900

    Fix: feat FloatTest

--- src/main/java/com/example/javaproject2/FloatTest.java
+++ src/main/java/com/example/javaproject2/FloatTest.java
@@ -6,14 +6,17 @@ public class FloatTest {
         float f8 = 1.23e8f;
         float f9 = 1.23e9f;
         float f10 = 1.23e10f;
-        float f11 = 3.141592653589793f;
+        float f11 = 3.141592f;
         double d10 = 1.23e10;
+        double d11 = 3.141592653589793;
+
         System.out.println("f7 = " + f7);
         System.out.println("f8 = " + f8);
         System.out.println("f9 = " + f9);
         System.out.println("f10 = " + f10);
         System.out.println("f11 = " + f11);
-        System.out.println("d1 = " + d10);
+        System.out.println("d10 = " + d10);
+        System.out.println("d11 = " + d11);
 
     }
 }
