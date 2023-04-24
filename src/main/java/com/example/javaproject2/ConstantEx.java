f79ac5db0b97d5ae70151b708a2e40dfb472e330
se-ize <seizenim@gmail.com>
2023-04-24 11:37:30 +0900

    Fix: add ConstantEx

---
+++ src/main/java/com/example/javaproject2/ConstantEx.java
@@ -0,0 +1,10 @@
+package com.example.javaproject2;
+
+public class ConstantEx {
+    public static void main(String[] args) {
+        final int iVal = 1;
+        System.out.println(iVal);
+
+        final PrintHello printHello = new PrintHello();
+    }
+}
---
+++ src/main/java/com/example/javaproject2/FloatTest.java
@@ -0,0 +1,20 @@
+package com.example.javaproject2;
+
+public class FloatTest {
+    public static void main(String[] args) {
+        float f7 = 1.23e7f;
+        float f8 = 1.23e8f;
+        float f9 = 1.23e9f;
+        float f10 = 1.23e10f;
+        float f11 = 3.141592653589793f;
+        double d10 = 1.23e10;
+
+        System.out.println("f7 = " + f7);
+        System.out.println("f8 = " + f8);
+        System.out.println("f9 = " + f9);
+        System.out.println("f10 = " + f10);
+        System.out.println("f11 = " + f11);
+        System.out.println("d1 = " + d10);
+
+    }
+}
