757f49c214416ff488131d6722e079cf8b742bec
se-ize <seizenim@gmail.com>
2023-04-25 14:37:38 +0900

    Fix: add ArraysToString

---
+++ src/main/java/com/example/javaproject2/ArraysToString.java
@@ -0,0 +1,13 @@
+package com.example.javaproject2;
+
+import java.util.Arrays;
+
+public class ArraysToString {
+    public static void main(String[] args) {
+        int[] iArr = new int[3];
+        iArr[0] = 2;
+        iArr[1] = 4;
+        iArr[2] = 7;
+        System.out.println(Arrays.toString(iArr));
+     }
+}
