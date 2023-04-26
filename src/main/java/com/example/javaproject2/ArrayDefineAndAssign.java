62427f23446c8975267286c645f6dca667451aab
se-ize <seizenim@gmail.com>
2023-04-26 13:07:17 +0900

    Fix: feat TwoDimArray

--- src/main/java/com/example/javaproject2/ArrayDefineAndAssign.java
+++ src/main/java/com/example/javaproject2/ArrayDefineAndAssign.java
@@ -12,5 +12,13 @@ public class ArrayDefineAndAssign {
         System.out.println(Arrays.toString(arr2[0]));
         System.out.println(Arrays.toString(arr2[1]));
         System.out.println(Arrays.toString(arr2[2]));
+
+        arr2[0][0] = 1;
+        arr2[0][2] = 2;
+        arr2[1][0] = 3;
+
+        System.out.println(Arrays.toString(arr2[0]));
+        System.out.println(Arrays.toString(arr2[1]));
+        System.out.println(Arrays.toString(arr2[2]));
     }
 }
---
+++ src/main/java/com/example/javaproject2/TwoDimArray.java
@@ -0,0 +1,26 @@
+package com.example.javaproject2;
+
+import java.util.Arrays;
+
+public class TwoDimArray {
+    public static void printArray(int[][] arr) {
+        System.out.println(Arrays.toString(arr[0]));
+        System.out.println(Arrays.toString(arr[1]));
+        System.out.println(Arrays.toString(arr[2]));
+    }
+
+    public static void main(String[] args) {
+        int[][] arr = new int[3][3];
+        printArray(arr);
+
+        arr[0][0] = 1;
+        arr[0][2] = 2;
+        arr[1][0] = 3;
+        printArray(arr);
+
+        arr[1][1] = 5;
+        printArray(arr);
+
+    }
+
+}
