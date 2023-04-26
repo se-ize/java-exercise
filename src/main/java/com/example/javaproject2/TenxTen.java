f12940380f40f4caab1b2f9002805ebe6f656170
se-ize <seizenim@gmail.com>
2023-04-26 10:31:09 +0900

    Fix: add TenxTen

---
+++ src/main/java/com/example/javaproject2/TenxTen.java
@@ -0,0 +1,12 @@
+package com.example.javaproject2;
+
+import java.util.Arrays;
+
+public class TenxTen {
+    public static void main(String[] args) {
+        int[][] arr10x10 = new int[10][10];
+        for(int i=0; i<10; i++){
+            System.out.println(Arrays.toString(arr10x10[i]));
+        }
+    }
+}
