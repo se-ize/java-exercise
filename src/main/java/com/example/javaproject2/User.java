62058becde7e230fc6aae86470d073ca7d8efc6a
se-ize <seizenim@gmail.com>
2023-04-26 09:39:02 +0900

    Fix: fix User

--- src/main/java/com/example/javaproject2/User.java
+++ src/main/java/com/example/javaproject2/User.java
@@ -4,6 +4,8 @@ public class User {
     String name;
     String phoneNumber;
     int age;
+    Boolean isBalanceSufficient = true;
+    Boolean isPaymentSuccess = false;
 
     boolean isAdult() {
         return age >= 18;
