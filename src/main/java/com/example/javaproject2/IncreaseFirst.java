0f6b0f4289b0978168975f4122c07d01d880c2a9
se-ize <seizenim@gmail.com>
2023-04-27 10:15:54 +0900

    Fix: add IncreaseFirst

---
+++ src/main/java/com/example/javaproject2/IncreaseFirst.java
@@ -0,0 +1,12 @@
+package com.example.javaproject2;
+
+public class IncreaseFirst {
+    public static void main(String[] args) {
+        int idx = 0;
+        int[] arr = {2, 1, 7, 9};
+        while(idx < 2) {
+            System.out.println(arr[idx]);
+            idx += 1;
+        }
+    }
+}
