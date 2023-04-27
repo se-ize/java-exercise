60d5a30df00d1b4d63f656b61cd4ac6c8a381ab0
se-ize <seizenim@gmail.com>
2023-04-27 10:41:08 +0900

    Fix: add ComparisonOperators

---
+++ src/main/java/com/example/javaproject2/ComparisonOperators.java
@@ -0,0 +1,15 @@
+package com.example.javaproject2;
+
+public class ComparisonOperators {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        System.out.printf("%d == %d --> %b\n", a, b, a == b);
+        System.out.printf("%d != %d --> %b\n", a, b, a != b);
+        System.out.printf("%d < %d --> %b\n", a, b, a < b);
+        System.out.printf("%d <= %d --> %b\n", a, b, a <= b);
+        System.out.printf("%d > %d --> %b\n", a, b, a > b);
+        System.out.printf("%d >= %d --> %b\n", a, b, a >= b);
+
+    }
+}
