9bee54ffc91a7d2ba935ecf95e83e192c02b4dfd
se-ize <seizenim@gmail.com>
2023-04-28 11:49:47 +0900

    Feat: add IfSwap

---
+++ src/main/java/com/example/javaproject2/IfSwap.java
@@ -0,0 +1,13 @@
+package com.example.javaproject2;
+import java.util.*;
+public class IfSwap {
+    public static void main(String[] args) {
+        int[] arr = {2, 1, 7, 9};
+        if(arr[0] > arr[1]) {
+            int temp = arr[0];
+            arr[0] = arr[1];
+            arr[1] = temp;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
