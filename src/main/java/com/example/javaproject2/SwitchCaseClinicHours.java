063fb49fc5506af423ebf6010a305b0f3dce8df0
se-ize <seizenim@gmail.com>
2023-04-28 13:49:44 +0900

    Feat: fix SwitchCaseClinicHours

--- src/main/java/com/example/javaproject2/SwitchCaseClinicHours.java
+++ src/main/java/com/example/javaproject2/SwitchCaseClinicHours.java
@@ -8,9 +8,8 @@ public class SwitchCaseClinicHours {
         String time = "휴진";
 
         switch(day) {
-            case "월", "화", "목", "금": time = "09:30-18:30"; break;
-            case "토": time = "09:30-13:00"; break;
-            case "수", "일": time = "휴진"; break;
+            case "월", "화", "수", "목", "금": time = "09:30-20:00"; break;
+            case "토", "일": time = "09:00-15:00"; break;
         }
         System.out.printf("%s요일은 %s입니다.", day, time);
     }
