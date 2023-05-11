5f6a17edc08b5546850bf9852a020979b6313904
se-ize <seizenim@gmail.com>
2023-05-11 09:48:49 +0900

    Feat: add GenericEx

---
+++ src/main/java/com/example/javaproject2/GenericEx.java
@@ -0,0 +1,13 @@
+package com.example.javaproject2;
+
+import java.util.*;
+
+public class GenericEx {
+    public static void main(String[] args) {
+        List<String> strList = new ArrayList<>();
+        strList.add("Hello");
+        strList.add("bye");
+        strList.add("1");
+
+    }
+}
---
+++ src/main/java/com/example/javaproject2/ListEx.java
@@ -0,0 +1,26 @@
+package com.example.javaproject2;
+
+import java.util.*;
+
+public class ListEx {
+    public static void main(String[] args) {
+        List l1 = new ArrayList();
+        l1.add("Hello");
+        l1.add(1);
+        System.out.println(l1.get(0));
+        System.out.println(l1.get(1));
+        System.out.println(l1.size());
+        System.out.println(l1.isEmpty());
+
+        System.out.println("-----------");
+        for(var item : l1) {
+            System.out.println(item);
+        }
+        System.out.println("-----------");
+
+        l1.remove(0);
+        l1.remove(0);
+        System.out.println(l1.isEmpty());
+
+    }
+}
