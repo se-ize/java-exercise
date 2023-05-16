99c027367f7c4287627e58dcf9f7853b5d3ec03a
se-ize <seizenim@gmail.com>
2023-05-16 10:07:18 +0900

    Feat: add BubbleSort

---
+++ src/main/java/com/example/javaproject2/BubbleSort01.java
@@ -0,0 +1,21 @@
+package com.example.javaproject2;
+
+import java.util.Arrays;
+
+public class BubbleSort01 {
+    public static void main(String[] args) {
+
+        int[] arr = {7, 2, 3, 9, 28, 11};
+
+        for (int j = 1; j < arr.length - 1; j++) {
+            for (int i = 0; i < arr.length - 1; i++) {
+                if (arr[i] > arr[i + 1]) {
+                    int temp = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = temp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
